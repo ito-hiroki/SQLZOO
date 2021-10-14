8682c81a72ddb3d39e13e8d808dc937970fe9672
Ito Hiroki <hiroki.i0307@gmail.com>
2021-10-14 16:59:35 +0900

    Add all ans of sec.8

--- 8_numeric_examples.sql
+++ 8_numeric_examples.sql
@@ -37,3 +37,57 @@ where
 group by
     subject
 ;
+
+-- 5
+select
+    subject, sum(A_STRONGLY_AGREE*response*0.01)
+from
+    nss
+where
+    question = 'Q22'
+    and (subject = '(8) Computer Science' or subject = '(H) Creative Arts and Design')
+group by
+    subject
+;
+
+-- 6
+select
+    subject, round(sum(A_STRONGLY_AGREE*response*0.01) / sum(response)*100, 0)
+from
+    nss
+where
+    question = 'Q22'
+    and (subject = '(8) Computer Science' or subject = '(H) Creative Arts and Design')
+group by
+    subject
+;
+
+-- 7
+select
+    institution, round(sum(score*response*0.01) / sum(response) * 100, 0)
+from
+    nss
+where
+    question = 'Q22'
+    and institution like '%Manchester%'
+group by
+    institution
+;
+
+-- 8
+select
+    institution, sum(sample), 
+    sum(
+        case subject
+            when subject = '(8) Computer Science' then 0
+            else sample
+        end
+    )
+from
+    nss
+where
+    question = 'Q01'
+    and institution like '%Manchester%'
+group by
+    institution
+;
